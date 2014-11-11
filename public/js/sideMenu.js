eb45b0472458dbbaa469c4280f1171e9922520ce
rieco <quit.replay@gmail.com>
2014-11-11 14:51:00 +0900

    ハンバーガー！！！

--- public/js/sideMenu.js
+++ public/js/sideMenu.js
@@ -4,9 +4,11 @@ $(function(){
     if (menuFlg) {
       $("#sideMenu").stop().animate({"left":"-250px"},500,"easeOutSine");
       menuFlg = false;
+      $("#open").removeClass('active');
     } else {
       $("#sideMenu").stop().animate({"left":"0px"},500,"easeOutSine");
       menuFlg = true;
+      $("#open").addClass('active');
     }
   });
 });
